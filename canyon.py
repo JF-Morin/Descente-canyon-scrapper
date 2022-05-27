35413690f650da06a704d8dc7cdb3e01ee2429f0
JF <jfmorincamo@gmail.com>
2022-05-27 19:08:04 -0400

    Trying to solve server connection rejection

--- canyon.py
+++ canyon.py
@@ -1,54 +1,50 @@
 # Imports
 from dataclasses import dataclass, replace
-from requests_html import HTMLSession
-
-
+import requests_html
 
 # Class: Canyon
 @dataclass
 class Canyon:
-    # Attributes
-    country: str = ''
-    state: str = ''
-    region: str = ''
-    city: str = ''
-    massif: str = ''
-    bassin: str = ''
-    water_stream: str = ''
-    interets: float = 0.0
-    starting_altitude: int = 0
-    elevation_drop: int = 0
-    length: int = 0
-    max_waterfall_height: int = 0
-    grade: str = ''
-    minimum_rope_length: int = 0
-    approach_time: str = ''
-    descent_time: str = ''
-    exit_time: str = ''
-    shuttle_distance: str = ''
-    access_description: str = ''
-    approach_description: str = ''
-    descent_description: str = ''
-    exit_description: str = ''
-    comments: str = ''
+    def __init__(self):
+        # Attributes
+        self.name: str = ''
+        self.country: str = ''
+        self.state: str = ''
+        self.region: str = ''
+        self.city: str = ''
+        self.massif: str = ''
+        self.bassin: str = ''
+        self.water_stream: str = ''
+        self.interest: float = 0.0
+        self.starting_altitude: int = 0
+        self.elevation_drop: int = 0
+        self.length: int = 0
+        self.max_waterfall_height: int = 0
+        self.grade: str = ''
+        self.minimum_rope_length: int = 0
+        self.approach_time: str = ''
+        self.descent_time: str = ''
+        self.exit_time: str = ''
+        self.shuttle_distance: str = ''
+        self.access_description: str = ''
+        self.approach_description: str = ''
+        self.descent_description: str = ''
+        self.exit_description: str = ''
+        self.comments: str = ''
+    
+    def __iter__(self):
+        return self
 
     # Methods
 
-    def fill_canyon_data(self, canyon_url):
-        print('\n' + canyon_url)
-        session = HTMLSession()
-        base_url = 'https://www.descente-canyon.com'
-        urls = []
-        request = session.get(canyon_url)
-
-        tab = request.html.find('.nav.nav-pills.onglets li')
-        if not tab:
-            return
-        tab_url = base_url + tab[1].find('a')[0].attrs['href']
-        description_html = session.get(tab_url)
+    def parse_html_canyon_data(self, response):
+        description_html = response.html
+        # Canyon name
+        self.name = description_html.find('.col-md-9')[0].find('h1')[0].search('<strong>{}</strong>')[0]
 
         # Location description
-        location_description = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')
+        location_description = description_html.find('.fichetechniqueintegree .list-group .list-group-item')
+
         if not location_description:
             return
         labels = ["Pays","Région","Département","Commune","Massif","Bassin", "Cours d'eau"]
@@ -60,53 +56,53 @@ class Canyon:
                 i += 1
 
         # Canyon Interest
-        interests = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[2].search('<strong>{}</strong>')
-        self.interets = 0.0 if interests == None else float(interests[0].replace(',','.'))
+        interest = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[2].search('<strong>{}</strong>')
+        self.interest = 0.0 if interest == None else float(interest[0].replace(',','.'))
 
         # Canyon Starting Altitude
-        starting_altitude = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[3].find('.badge')[0]
+        starting_altitude = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[3].find('.badge')[0]
         self.starting_altitude = 0 if starting_altitude.text == "??" else int(starting_altitude.text.replace('Alti dép. ','').replace('m',''))
 
         # Canyon Elevation Drop
-        elevation_drop = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[4].find('.badge')[0]
+        elevation_drop = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[4].find('.badge')[0]
         self.elevation_drop = 0 if elevation_drop.text == "??" else int(elevation_drop.text.replace('Dénivelé ','').replace('m',''))
 
         # Canyon Length
-        length = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[5].find('.badge')[0]
+        length = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[5].find('.badge')[0]
         self.length = 0 if length.text == "??" else int(length.text.replace('Longueur ','').replace('m',''))
 
         # Canyon Max Waterfall Height
-        max_waterfall_height = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[6].find('.badge')[0]
+        max_waterfall_height = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[6].find('.badge')[0]
         self.max_waterfall_height = 0 if max_waterfall_height.text == "??" else int(max_waterfall_height.text.replace('Casc. max ','').replace('m',''))
 
         # Canyon Grade
-        grade = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[7].find('.badge')[0]
+        grade = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[7].find('.badge')[0]
         self.grade = "" if grade.text == "??" else grade.text.replace('Cotation ','')
 
         # Canyon Minimum Rope Length
-        minimum_rope_length = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[8].find('.badge')[0]
+        minimum_rope_length = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[8].find('.badge')[0]
         self.minimum_rope_length = 0 if minimum_rope_length.text == "??" else int(minimum_rope_length.text.replace('Corde ','').replace('m','').replace('*',''))
 
         # Canyon Approach Time
-        approach_time = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[9].find('.badge')[0]
+        approach_time = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[9].find('.badge')[0]
         self.approach_time = "" if approach_time.text == "??" else approach_time.text.replace('Approche: ','')
 
         # Canyon Descent Time
-        descent_time = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[10].find('.badge')[0]
+        descent_time = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[10].find('.badge')[0]
         self.descent_time = "" if descent_time.text == "??" else descent_time.text.replace('Descente: ','')
 
         # Canyon Exit Time
-        exit_time = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[11].find('.badge')[0]
+        exit_time = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[11].find('.badge')[0]
         self.exit_time = "" if exit_time.text == "??" else exit_time.text.replace('Retour: ','')
 
         # Canyon Shuttle Distance
-        shuttle_distance = description_html.html.find('.fichetechniqueintegree .list-group .list-group-item')[12].find('.badge')[0]
+        shuttle_distance = description_html.find('.fichetechniqueintegree .list-group .list-group-item')[12].find('.badge')[0]
         self.shuttle_distance = "" if shuttle_distance.text == "??" else shuttle_distance.text.replace('Navette: ','')
 
 
         # Text description
-        h3_elements = description_html.html.find('.nav.nav-pills.onglets + div')[0].find('h3')
-        p_elements = description_html.html.find('.nav.nav-pills.onglets + div')[0].find('p')
+        h3_elements = description_html.find('.nav.nav-pills.onglets + div')[0].find('h3')
+        p_elements = description_html.find('.nav.nav-pills.onglets + div')[0].find('p')
         for index,h3 in enumerate(h3_elements):
             if h3.text == 'Accès':
                 self.access_description = p_elements[index].text
@@ -116,23 +112,7 @@ class Canyon:
                 self.descent_description = p_elements[index].text
             elif h3.text == 'Engagement':
                 self.exit_description = p_elements[index].text
-
-
         
-        # print("Interest: " + str(self.interets) + '/4')
-        # print("Sarting elevation: " + str(self.starting_altitude))
-        # print("Elevaton drop: " + str(self.elevation_drop))
-        # print("Length: " + str(self.length))
-        # print("Max Waterfall height: " + str(self.max_waterfall_height))
-        # print("Grade: " + self.grade)
-        # print("Min Rope Length: " + str(self.minimum_rope_length))
-        # print("Approach: " + self.approach_time)
-        # print("Descent: " + self.descent_time)
-        # print("Exit: " + self.exit_time)
-        # print("Shuttle: " + self.shuttle_distance)
-        # print('')
-   
-
         return
 
     
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Imports
 import sys
 from requests_html import HTMLSession
-import json
+import csv
+import time
+
 from canyon import Canyon
 
 session = HTMLSession()
-
 
 def get_country_links():
     urls = []
@@ -19,7 +20,7 @@ def get_country_links():
         urls.append(url)
     return urls
 
-def  get_canyon_list(url):
+def get_canyon_list(url):
     base_url = 'https://www.descente-canyon.com'
     canyon_list = []
     # Get Tab URL
@@ -75,6 +76,16 @@ def get_secondary_site_list(url):
         secondary_site_list.append(new_url.replace("'",""))
     return secondary_site_list
 
+def get_canyon_tab_url(url):
+    base_url = 'https://www.descente-canyon.com'
+    request = session.get(url)
+    tab = request.html.find('.nav.nav-pills.onglets li')
+    if not tab:
+        return ''
+    tab_url = base_url + tab[1].find('a')[0].attrs['href']
+    # description_html = session.get(tab_url)
+    return tab_url
+
 def main():
     canyons = []
     canyon_urls = []
@@ -82,18 +93,82 @@ def main():
     sys.stdout.write('Countries: ' + str(len(country_urls)))
     sys.stdout.write('\n')
     sys.stdout.flush()
+
+    # Get canyon urls
     for url in country_urls:
         canyon_urls += get_canyon_list(url)
         canyon_urls += get_secondary_site_list(url)
         sys.stdout.write('\rTotal canyons: ' + str(len(canyon_urls)))
         sys.stdout.flush()
     sys.stdout.write('\n')
-    for x in range (4):
+
+    # Get canyons data
+    for canyon_url in canyon_urls:
         canyon = Canyon()
-        canyon.fill_canyon_data(canyon_urls[x])
-        canyons += canyon
+        response = ''
+        time_to_wait = 5
+        resp_count = 0
+        while response == '':
+            try:
+                if resp_count >=100:
+                    time.sleep(time_to_wait)
+                    resp_count = 0
+                tab_url = get_canyon_tab_url(canyon_url)
+                response = session.get(tab_url)
+                break
+            except:
+                print("Connection refused by the server: sleep for " + str(time_to_wait) + " seconds")
+                # session.close()
+                # session = None
+                # session = HTMLSession()
+                time.sleep(time_to_wait)
+                time_to_wait += 5
+                continue
+
+        # tab_url = get_canyon_tab_url(canyon_url)
+        # response = session.get(tab_url)
+        
+        canyon.parse_html_canyon_data(response)
+        canyons.append(canyon)
         sys.stdout.write('\rGet info canyon: ' +str(len(canyons)) + ' of ' + str(len(canyon_urls)))
         sys.stdout.flush()
+    
+    # Save canyons to CSV file
+    headers = ['Name','Country','State','Region','City','Massif','Bassin','Water Stream','Interest','Starting Altitude','Elevation Drop','Length','Max Waterfall Height','Grade','Minimum Rope Length','Approach Time','Descent Time','Exit Time','Shuttle Distance','Access Description','Approach Description','Descent Description','Exit Description']
+    with open('Canyons_DB.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(headers)
+
+        # write the data
+        for canyon in canyons:
+            data = [canyon.name,
+                    canyon.country,
+                    canyon.state,
+                    canyon.region,
+                    canyon.city,
+                    canyon.massif,
+                    canyon.bassin,
+                    canyon.water_stream,
+                    canyon.interest,
+                    canyon.starting_altitude,
+                    canyon.elevation_drop,
+                    canyon.length,
+                    canyon.max_waterfall_height,
+                    canyon.grade,
+                    canyon.minimum_rope_length,
+                    canyon.approach_time,
+                    canyon.descent_time,
+                    canyon.exit_time,
+                    canyon.shuttle_distance,
+                    canyon.access_description,
+                    canyon.approach_description,
+                    canyon.descent_description,
+                    canyon.exit_description]
+            writer.writerow(data)
+
+    
 
 if __name__ == '__main__':
     main()
